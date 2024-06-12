b39472f2fef967896ff11e4e3ff060dd13b97a98
jacky-ui <jacky.du.004@gmail.com>
2024-06-12 18:42:17 -0400

    i react-countup and create animatedcounter to work

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,17 @@
+'use client';
+import React from 'react';
+import CountUp from 'react-countup';
+
+const AnimatedCounter = ({ amount }: {amount: number}) => {
+  return (
+    <div>
+        <CountUp 
+            end={amount} 
+            decimal=","
+            prefix="$"
+        />
+    </div>
+  )
+}
+
+export default AnimatedCounter
--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { formatAmount } from '@/lib/utils';
+import AnimatedCounter from './AnimatedCounter';
 
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
@@ -14,7 +14,11 @@ const TotalBalanceBox = ({
             <h2 className='header-2'>Bank Accounts: {totalBanks}</h2>
             <div className='flex flex-col gap-2'>
                 <p className='total-balance-label'>Total Current Balance</p>
-                <p className='total-balance-amount flex-center gap-2'>{formatAmount(totalCurrentBalance)}</p>
+                <p className='total-balance-amount flex-center gap-2'>
+                    <AnimatedCounter 
+                        amount={totalCurrentBalance}
+                    />
+                </p>
             </div>
         </div>
     </section>
